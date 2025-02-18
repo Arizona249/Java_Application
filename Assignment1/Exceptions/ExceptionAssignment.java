fef5a584b33aef029aab194879afecd7ed538775
Arizona249 <arinzlotanna705@gmail.com>
2025-02-18 21:27:29 +1300

    Assignment

---
+++ Assignment1/Exceptions/ExceptionAssignment.java
@@ -0,0 +1,17 @@
+public class ExceptionAssignment{
+	public static void main(String[]args){
+		// The different exceptions in the Exception class are
+		// IOException
+		// SQLException
+		// ArrayIndexOutOfBoundsException
+		// ClassCastException
+		// IllegalArgumentException
+		// IndexOutOfBoundsException
+		// NullPointerException
+		// NumberFormatException
+		// StringIndexOutOfBoundsException
+		// UnsupportedOperationException
+
+		
+	}
+}
